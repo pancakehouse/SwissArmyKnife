9e2759dbe554c4c2b94b4bdf14939062d2b6d686
techtideapps <ngable1995@gmail.com>
2017-02-20 19:28:44 -0600

    Fixed all strobelight issues

--- app/src/main/java/com/techtideapps/swissarmyknife/Flashlight.java
+++ app/src/main/java/com/techtideapps/swissarmyknife/Flashlight.java
@@ -42,7 +42,7 @@ public class Flashlight extends AppCompatActivity {
             isFlashOn = false;
         }
     }
-
+    StrobeThread thread;
     public void strobe(View view) throws InterruptedException {
         SeekBar bar = (SeekBar) findViewById(R.id.seekBar2);
         if(isFlashOn == true){
@@ -51,16 +51,46 @@ public class Flashlight extends AppCompatActivity {
         }
         isFlashOn = false;
         //Should be in a new thread
-        Camera.open();
+        if(isStrobeOn == false){
+            thread = new StrobeThread(bar, false);
+            thread.start();
+            isStrobeOn = true;
+        } else{
+            thread.end = true;
+            isStrobeOn = false;
+        }
+    }
+}
+
+class StrobeThread extends Thread{
+    SeekBar bar;
+    boolean end = false;
+    StrobeThread(SeekBar bar2, boolean end2){
+        bar = bar2;
+        end = end2;
+    }
+    public void run(){
+        Camera camera;
+        camera = Camera.open();
         camera.startPreview();
         Camera.Parameters parameters = camera.getParameters();
-        while(true){
+        while(!end){
             parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
             camera.setParameters(parameters);
-            Thread.sleep(500);
+            try {
+                Thread.sleep(1000 / (bar.getProgress() + 1));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
             camera.setParameters(parameters);
-            Thread.sleep(500);
+            try {
+                Thread.sleep(1000 / (bar.getProgress() + 1));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        camera.stopPreview();
+        camera.release();
     }
 }
